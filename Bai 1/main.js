1a4b7474799046f8a6e302b9923e7598eab5f42f
nhuttan12 <144250758+nhuttan12@users.noreply.github.com>
2023-10-13 21:51:42 +0700

    upload bài 1 lab 4
    upload bài 1 lab 4 lần đầu và chưa có lần chỉnh sửa nào

---
+++ Bai 1/main.js
@@ -0,0 +1,9 @@
+function change_color_to_yellow(){
+    document.body.style.backgroundColor='yellow';
+}
+function change_color_to_blue(){
+    document.body.style.backgroundColor='blue';
+}
+function change_color_to_red(){
+    document.body.style.backgroundColor='red';
+}
